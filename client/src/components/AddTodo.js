4660effac9e40e398ee6568be5d855f7eea0dabb
munjuin <raiawa1212@gmail.com>
2024-08-14 18:57:58 +0900

    study: mini-todo-project

---
+++ client/src/components/AddTodo.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+export default function AddTodo({ addItem }) {
+  const [todoItem, setTodoItem] = useState({
+    title: "",
+  }); // 사용자 입력을 저장할 객체 (id, title, done에 대한 정보를 저장해야하므로 객체)
+
+  const onButtonClick = () => {
+    addItem(todoItem); // add 함수 사용.
+    setTodoItem({
+      title: "", // 상태 초기화
+    });
+  };
+
+  const enterKeyEventHandler = (e) => {
+    if (e.key === "Enter") {
+      setTodoItem(true);
+      setTodoItem(e.target.value);
+    }
+  };
+  return (
+    <div className="AddTodo">
+      <input
+        type="text"
+        placeholder="Add your new Todo"
+        value={todoItem.title}
+        onChange={(e) => setTodoItem({ title: e.target.value })}
+        onKeyDown={enterKeyEventHandler}
+      />
+      <button onClick={onButtonClick}>ADD</button>
+    </div>
+  );
+}
---
+++ server/init.sql
@@ -0,0 +1,50 @@
+create database codingon default character set utf8 collate utf8_general_ci
+-- 데이터베이스 목록 확인
+SHOW databases;
+
+-- codingon 데이터베이스 선택
+USE codingon;
+
+-- codingon 데이터베이스의 테이블 목록 확인
+SHOW tables;
+
+-- 이미 todo 테이블이 있다면 기존 테이블 지움
+DROP TABLE if exists todo;
+
+-- todo 테이블 생성
+create table todo(
+
+	id int not null primary key auto_increment,
+  title varchar(20) not null,
+  done boolean not null default false
+      
+);
+
+
+
+-- todo 테이블 데이터 추가
+INSERT INTO todo values (null, 'my todo1', 0);
+INSERT INTO todo values (null, 'my todo2', 0);
+INSERT INTO todo values (null, 'my todo3', 0);
+INSERT INTO todo values (null, 'my todo4', 1);
+INSERT INTO todo values (null, 'my todo5', 1);
+INSERT INTO todo values (null, 'my todo6', 1);
+
+-- todo 테이블 구조 보기
+DESC todo; 
+
+-- todo 테이블 보기
+select * from todo;
+
+update todo set title = '내가 할일 2번' where id = 2;
+
+delete from todo where id = 3;
+
+select * from mysql.user;
+
+create user 'user'@'%' identified by '1234';
+create user 'user'@'%' identified With mysql_native_password by '1234';
+
+grant all privileges on *.* to 'user'@'%' with grant option;
+
+flush privileges;
